4152d0d5a60ad2b881b504236da59a6d70914409
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-03-14 23:16:23 +0100

    Update main func

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
-import (
-	"log"
-)
+func main() {
 
-var (
-	archmodel adm
-	predictor map[string]Predictor
-)
+	// Read and create adm from file
+
+	// Create fpm
+
+	// start reading new data from influxdb every 1 min and push to channel mondatch
+
+	// go routine
+	// read new data from channel
+	// make prediction for that component
+	// update prob
+	// update fpm (with delay)
+	// push result to influxdb
+}
